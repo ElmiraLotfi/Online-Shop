f0c46cfbf0c308ac90ea83bb59d0a296afd749fc
Elmira Lotfi <elmiralotfi1375@gmail.com>
2024-05-13 11:26:54 +0330

    Create a search field

--- vite-project/src/pages/ProductsPage.jsx
+++ vite-project/src/pages/ProductsPage.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
+//context
 import { useProducts } from "../context/ProductContext"
 //components
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 //styles
 import styles from "./ProductsPage.module.css";
-
+//icons
+import { ImSearch } from "react-icons/im";
 
 function ProductsPage() {
   const products = useProducts();
   console.log(products);
+
+  const [search,setSearch]=useState("");
+
+  const searchHandeler = ()=>{
+    console.log(search);
+  }
   return (
+    <>
+    <div>
+    <input 
+    type="text" 
+    placeholder="search..."
+    value={search}
+    onChange={e=>setSearch(e.target.value.toLowerCase().trim())}
+    />
+    <button onClick={searchHandeler}><ImSearch /></button>
+    </div>
     <div className={styles.container}>
     <div className={styles.products}>
       {!products.length && <Loader/>}
@@ -20,6 +39,7 @@ function ProductsPage() {
     </div>
     <div>sidebar</div>
     </div>
+    </>
   )
 }
 
